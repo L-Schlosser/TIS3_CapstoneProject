c3b2bb09adedbb06ec36836abb395f4e36d6528d
Jonas Welt <57290928+JW-200@users.noreply.github.com>
2025-12-28 20:38:39 +0100

    added all metrics

--- code/utils/metrics.py
+++ code/utils/metrics.py
@@ -3,13 +3,20 @@ import pandas as pd
 from sklearn.metrics import mean_absolute_error, root_mean_squared_error, mean_absolute_percentage_error
 
 if __package__:
-    from .constants import FREQ_DAILY, FREQ_MONTHLY, METRICS_DIR, FAMILY_BASELINE
+    from .constants import FREQ_DAILY, FREQ_MONTHLY, METRICS_DIR, FAMILY_BASELINE, FAMILY_STATISTICAL, FAMILY_MACHINE_LEARNING, FAMILY_DEEP_LEARNING
     from .preprocessing import load_daily_data, load_monthly_data
     from .baseline import run_baseline_forecast_daily, run_baseline_forecast_monthly
+    from .statistical import run_statistical_forecast_daily, run_statistical_forecast_monthly
+    from .machine_learning import run_machine_learning_forecast_daily, run_machine_learning_forecast_monthly
+    from .deep_learning import run_deep_learning_forecast_daily, run_deep_learning_forecast_monthly
 else:
-    from constants import FREQ_DAILY, FREQ_MONTHLY, METRICS_DIR, FAMILY_BASELINE
+    from constants import FREQ_DAILY, FREQ_MONTHLY, METRICS_DIR, FAMILY_BASELINE, FAMILY_STATISTICAL, FAMILY_MACHINE_LEARNING, FAMILY_DEEP_LEARNING
     from preprocessing import load_daily_data, load_monthly_data
     from baseline import run_baseline_forecast_daily, run_baseline_forecast_monthly
+    from statistical import run_statistical_forecast_daily, run_statistical_forecast_monthly
+    from machine_learning import run_machine_learning_forecast_daily, run_machine_learning_forecast_monthly
+    from deep_learning import run_deep_learning_forecast_daily, run_deep_learning_forecast_monthly
+
 
 def _calculate_metric_monthly(prediction_df: pd.DataFrame, family: str, frequency: str) -> pd.DataFrame:
     """Calculate evaluation metrics for monthly frequency."""
@@ -37,7 +44,7 @@ def _calculate_metric_monthly(prediction_df: pd.DataFrame, family: str, frequenc
 
     return pd.DataFrame(metrics).sort_values(by=["MAPE"])
 
-def calculate_metrics(prediction_df: pd.DataFrame, family: str, frequency: str, use_existing: bool) -> pd.DataFrame:
+def calculate_metrics(prediction_df: pd.DataFrame, family: str, frequency: str, use_existing: bool = True) -> pd.DataFrame:
     """Calculate evaluation metrics for the predictions."""
     if frequency != FREQ_DAILY and frequency != FREQ_MONTHLY:
         raise ValueError(f"Unsupported frequency: {frequency}")
@@ -60,11 +67,23 @@ def calculate_metrics(prediction_df: pd.DataFrame, family: str, frequency: str, 
 
 if __name__ == "__main__":
     train, val, test = load_daily_data(use_existing=True)
-    val_fc, test_fc = run_baseline_forecast_daily(train, val, test, use_existing=True)
     train_m, val_m, test_m = load_monthly_data(use_existing=True)
-    val_m_fc, test_m_fc = run_baseline_forecast_monthly(train_m, val_m, test_m, use_existing=True)
+    val_base, test_base = run_baseline_forecast_daily(train, val, test, use_existing=True)
+    val_stat, test_stat = run_statistical_forecast_daily(train, val, test, use_existing=True)
+    val_ml, test_ml = run_machine_learning_forecast_daily(train, val, test, use_existing=True)
+    val_dl, test_dl = run_deep_learning_forecast_daily(train, val, test, use_existing=True)
 
-    calculate_metrics(val_fc, FAMILY_BASELINE, FREQ_DAILY, use_existing=False)
-    calculate_metrics(val_fc, FAMILY_BASELINE, FREQ_DAILY, use_existing=True)
-    calculate_metrics(val_m_fc, FAMILY_BASELINE, FREQ_MONTHLY, use_existing=False)
-    calculate_metrics(val_m_fc, FAMILY_BASELINE, FREQ_MONTHLY, use_existing=True)
+    calculate_metrics(val_base, FAMILY_BASELINE, FREQ_DAILY, use_existing=False)
+    calculate_metrics(val_stat, FAMILY_STATISTICAL, FREQ_DAILY, use_existing=False)
+    calculate_metrics(val_ml, FAMILY_MACHINE_LEARNING, FREQ_DAILY, use_existing=False)
+    calculate_metrics(val_dl, FAMILY_DEEP_LEARNING, FREQ_DAILY, use_existing=False)
+
+    val_m_base, test_m_base = run_baseline_forecast_monthly(train_m, val_m, test_m, use_existing=True)
+    val_m_stat, test_m_stat = run_statistical_forecast_monthly(train_m, val_m, test_m, use_existing=True)
+    val_m_ml, test_m_ml = run_machine_learning_forecast_monthly(train_m, val_m, test_m, use_existing=True)
+    val_m_dl, test_m_dl = run_deep_learning_forecast_monthly(train_m, val_m, test_m, use_existing=True)
+
+    calculate_metrics(val_m_base, FAMILY_BASELINE, FREQ_MONTHLY, use_existing=False)
+    calculate_metrics(val_m_stat, FAMILY_STATISTICAL, FREQ_MONTHLY, use_existing=False)
+    calculate_metrics(val_m_ml, FAMILY_MACHINE_LEARNING, FREQ_MONTHLY, use_existing=False)
+    calculate_metrics(val_m_dl, FAMILY_DEEP_LEARNING, FREQ_MONTHLY, use_existing=False)
