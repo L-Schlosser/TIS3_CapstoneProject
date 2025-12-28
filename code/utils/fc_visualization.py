a53e90b47a97de6ca4d16e1a29e0eacb1525ff7f
L-Schlosser <lorenz.schlosser@gmx.net>
2025-12-28 20:15:56 +0100

    visualizations
    - save pre visualizations
    - create py file for fc vis

---
+++ code/utils/fc_visualization.py
@@ -0,0 +1,24 @@
+#CODE FOR VISUALIZATIONS:
+
+## Visualizations for:
+# Best model
+# Best model / category
+# Best 3 models
+# Daily / monthly compare
+# Error of best model
+
+
+def plot_best_model():
+    pass
+
+def plot_best_model_per_category():
+    pass
+
+def plot_best_x_models(x=3):
+    pass 
+
+def plot_daily_monthly_comparison():
+    pass
+
+def plot_error_of_best_model():
+    pass
